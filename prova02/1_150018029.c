75047aed7ff2ed3054f8d93f855fdd6ae6f94d00
matheusbsilva <matheusbattista@hotmail.com>
2016-11-03 18:33:09 -0200

    Questão 1 Prova 02

--- prova02/1_150018029.c
+++ prova02/1_150018029.c
@@ -4,11 +4,11 @@
 
 int verifica(char *s){
     char *pilha;
-    int t,n,i;
+    int t,n,i=0;
     n=strlen(s);
     pilha=(char *)malloc(n*sizeof(char));
-    t=0;//pilha vazia
-    for(i=0;s[i]!='\0';i++){
+    t=0;
+    while(s[i]!='\0'){
         switch(s[i]){
             case ')':if(t!=0 && pilha[t-1]=='(')
                         --t;
@@ -20,13 +20,28 @@ int verifica(char *s){
                     else
                         return 0;
                     break;
+            case '}':if(t!=0 && pilha[t-1]=='{')
+                      --t;
+                    else
+                      return 0;
+                    break;
+            case ' ':break;
+            case '\n':break;
             default: pilha[t++]=s[i];
         }
+        i++;
     }
     return t==0;
 
 }
 int main(){
+  char *entrada=(char *)calloc(200,sizeof(char));
+  fgets(entrada,200,stdin);
 
-    return 0;
+  if(verifica(entrada))
+    printf("YES\n" );
+  else
+    printf("NO\n" );
+
+  return 0;
 }
