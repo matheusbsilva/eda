36572461c59d56954463b61d26294701f94ae49b
matheusbsilva <matheusbattista@hotmail.com>
2016-12-05 23:43:04 -0200

    Balanceamento de Arvore

--- arvore/arvore_29_11.c
+++ arvore/arvore_29_11.c
@@ -21,6 +21,36 @@ int altura(Arvore *raiz){
         return 0;
 }
 
+Arvore *rod_esq(Arvore *raiz){
+  Arvore *q,*temp;
+  q=raiz->dir;
+  temp=q->esq;
+  q->esq=raiz;
+  raiz->dir=temp;
+  q->fatorb=altura(raiz->esq)-altura(raiz->dir);
+  return q;
+}
+
+Arvore *rod_dir(Arvore *raiz){
+  Arvore *q,*temp;
+  q=raiz->esq;
+  temp=q->dir;
+  q->dir=raiz;
+  raiz->esq=temp;
+  q->fatorb=altura(raiz->esq)-altura(raiz->dir);
+  return q;
+}
+
+Arvore *rod_dir_esq(Arvore *raiz){
+  raiz->dir=rod_dir(raiz->dir);
+  return rod_esq(raiz);
+}
+
+Arvore *rod_esq_dir(Arvore *raiz){
+  raiz->esq=rod_esq(raiz->esq);
+  return rod_dir(raiz);
+}
+
 Arvore *inserir(Arvore *raiz,int info){
     if(!raiz){
         raiz=(Arvore*)malloc(sizeof(Arvore));
@@ -39,7 +69,64 @@ Arvore *inserir(Arvore *raiz,int info){
         raiz->fatorb=altura(raiz->esq)-altura(raiz->dir);
 
     }
+
+    if(raiz->fatorb > 1){
+      if(raiz->esq->fatorb < 0){
+        raiz=rod_esq_dir(raiz);
+      }
+      else{
+        raiz=rod_dir(raiz);
+      }
+    }
+    if(raiz->fatorb < -1){
+      if(raiz->dir->fatorb > 0){
+        raiz=rod_dir_esq(raiz);
+      }
+      else{
+        raiz=rod_esq(raiz);
+      }
+    }
     return raiz;
+}
+
+Arvore *busca(Arvore *raiz,int x){
+  if(raiz->info == x|| raiz==NULL)
+    return raiz;
+  else if(raiz->info < x)
+    return busca(raiz->dir,x);
+  else
+    return busca(raiz->esq,x);
+}
+
+Arvore *menorDireita(Arvore *raiz){
+  Arvore *atual=raiz;
+  while(atual->esq!=NULL){
+    atual=atual->esq;
+  }
+  return atual;
+}
+
+Arvore *deleta(Arvore *raiz){
+  if(raiz==NULL)
+    return raiz;
+  else{
+
+    if(raiz->dir==NULL){
+      Arvore *temp=raiz->esq;
+      free(raiz);
+      return temp;
+    }
+    else if(raiz->esq==NULL){
+      Arvore *temp=raiz->dir;
+      free(raiz);
+      return temp;
+    }
+
+    Arvore *temp=menorDireita(raiz->dir);
+    raiz->info=temp->info;
+    raiz->dir=deleta(busca(raiz->dir,temp->info));
+  }
+  return raiz;
 }
 
 void imprimir_ord(Arvore *raiz){
@@ -66,7 +153,7 @@ int balanceada(Arvore *raiz){
 
 int main(){
     Arvore *raiz=NULL;
-    int n,i,x;
+    int n,i,x,d;
     scanf("%d",&n);
 
     for (i = 0; i < n; i++) {
@@ -75,9 +162,30 @@ int main(){
     }
     imprimir_ord(raiz);putchar('\n');
 
-    if(balanceada(raiz))
+    if(balanceada(raiz)){
+
       printf("Esta balanceada\n");
-    else
+    }
+    else{
+
       printf("Nao esta balanceada\n");
+    }
+
+
+
+    scanf("%d",&d );
+    deleta(busca(raiz,d));
+
+    imprimir_ord(raiz);putchar('\n');
+
+    if(balanceada(raiz)){
+
+      printf("Esta balanceada\n");
+    }
+    else{
+
+      printf("Nao esta balanceada\n");
+    }
+
     return 0;
 }
